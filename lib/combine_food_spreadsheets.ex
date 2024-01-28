92ca29e3b60062061c11c180482523b0b47f5cc5
Philip Vegdahl <pvegdahl@gmail.com>
2024-01-27 21:43:43 -0500

    Parse one sheet down to something useful

--- lib/combine_food_spreadsheets.ex
+++ lib/combine_food_spreadsheets.ex
@@ -17,4 +17,41 @@ defmodule CombineFoodSpreadsheets do
     |> Enum.map(&Keyword.get(&1, :name))
     |> Enum.find_index(&(&1 == "Data"))
   end
+
+  def parse_table_data(table_data) do
+    table_data
+    |> Enum.reject(&list_starts_with_nil_or_empty?/1)
+    |> Enum.chunk_by(&List.first/1)
+    |> Enum.chunk_every(2)
+    |> Enum.map(&parse_one_sample/1)
+  end
+
+  defp list_starts_with_nil_or_empty?([nil | _]), do: true
+  defp list_starts_with_nil_or_empty?([]), do: true
+  defp list_starts_with_nil_or_empty?(_), do: false
+
+  defp parse_one_sample([[header], rows]) do
+    header_indices = find_header_indices(header)
+    name = sample_name(rows)
+
+    {name, parse_rows(rows, header_indices)}
+  end
+
+  defp find_header_indices(["Sample" | _] = header) do
+    day_index = Enum.find_index(header, &(&1 == "Day"))
+    duplicate_index = Enum.find_index(header, &(&1 == "Duplicate"))
+    log_index = Enum.find_index(header, &(&1 == "Log"))
+
+    [day: day_index, duplicate: duplicate_index, log: log_index]
+  end
+
+  defp sample_name([[name | _] | _] = _rows), do: name
+
+  defp parse_rows(rows, header_indices) do
+    Enum.map(rows, &parse_row(&1, header_indices))
+  end
+
+  defp parse_row(row, day: day_index, duplicate: duplicate_index, log: log_index) do
+    {Enum.at(row, day_index), Enum.at(row, duplicate_index), Enum.at(row, log_index)}
+  end
 end
--- test/combine_food_spreadsheets_test.exs
+++ test/combine_food_spreadsheets_test.exs
@@ -2,6 +2,28 @@ defmodule CombineFoodSpreadsheetsTest do
   use ExUnit.Case
   doctest CombineFoodSpreadsheets
 
-  test "greets the world" do
+  @table_data [
+    [nil, nil, "title", nil, nil, nil],
+    ["Sample", "Duplicate", "Day", "Other 1", "Log", "Other 2"],
+    ["Drunken Goat", 1, 0, 123, 3.21, 456],
+    ["Drunken Goat", 2, 0, 123, 3.24, 456],
+    ["Drunken Goat", 1, 1, 123, 7.65, 456],
+    ["Drunken Goat", 2, 1, 123, 7.61, 456],
+    [nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, nil, nil, nil],
+    ["Sample", "Duplicate", "Day", "Other 1", "Log", "Other 2"],
+    ["Soft Gouda", 1, 0, 123, 1.21, 456],
+    ["Soft Gouda", 2, 0, 123, 1.24, 456],
+    ["Soft Gouda", 1, 3, 123, 2.65, 456],
+    ["Soft Gouda", 2, 3, 123, 2.61, 456],
+    [nil, nil, nil, nil, nil, nil],
+    [nil, nil, nil, nil, nil, nil]
+  ]
+
+  test "Parse file into groups of cheese to data" do
+    assert CombineFoodSpreadsheets.parse_table_data(@table_data) == [
+             {"Drunken Goat", [{0, 1, 3.21}, {0, 2, 3.24}, {1, 1, 7.65}, {1, 2, 7.61}]},
+             {"Soft Gouda", [{0, 1, 1.21}, {0, 2, 1.24}, {3, 1, 2.65}, {3, 2, 2.61}]}
+           ]
   end
 end
